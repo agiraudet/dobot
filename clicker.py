4512d942899afbaec7a4c5bc90f4ca68b8f1d442
agiraude <agiraude@student.42.fr>
2023-07-18 21:39:31 +0200

    wip autopilot

--- clicker.py
+++ clicker.py
@@ -10,6 +10,7 @@ import re
 
 class Region:
     def __init__(self, x, y, w, h):
+        print("[Reg]Init...")
         self.x = x
         self.y = y
         self.w = w
@@ -28,7 +29,7 @@ class Region:
         print("x:{} y:{} w:{} h:{}".format(self.x, self.y, self.w, self.h))
 
     def screenshot(self, save=''):
-        if len(save):
+        if len(save) > 0:
             return pyautogui.screenshot(save, region=(
                 self.x,
                 self.y,
@@ -46,6 +47,7 @@ class Region:
 
 class Game:
     def __init__(self):
+        print("[Game]Init...")
         discoLoc = pyautogui.locateOnScreen('beacons/disco_btn.png')
         chatLoc = pyautogui.locateOnScreen('beacons/chat_btn.png')
         if discoLoc is None or chatLoc is None:
@@ -78,7 +80,7 @@ class Game:
         if len(matches) != 1:
             return None
         coord = matches[0].split(',')
-        return coord
+        return (int(coord[0]), int(coord[1]))
 
     def lookatCoord(self):
         coord_capt = self.coordRegion.screenshot()
@@ -124,12 +126,16 @@ class Game:
         return grouped_points
 
     def locateCompasses(self):
-        scr1 = self.region.screenshot()
         pyautogui.moveTo(self.region.x + self.region.w / 2,
                          self.region.y + self.region.h / 2)
+        pyautogui.keyDown('e')
+        scr1 = self.region.screenshot()
+        pyautogui.keyUp('e')
         pyautogui.press('a')
         time.sleep(1)
+        pyautogui.keyDown('e')
         scr2 = self.region.screenshot()
+        pyautogui.keyUp('e')
         sc1 = numpy.array(scr1)
         sc2 = numpy.array(scr2)
 
@@ -156,15 +162,26 @@ class Game:
         # cv2.imwrite('highlighted_elements.png', threshold)
 
     def findCompassDir(self, compass):
-        if compass[1] < self.cellH * 2 + 10:
+        if compass[1] < self.cellH * 2 + 20:
             return 'N'
-        if compass[1] > self.region.h - self.cellH - 10:
+        if compass[1] > self.region.h - self.cellH - 20:
             return 'S'
-        if compass[0] < self.cellW + 10:
+        if compass[0] < self.cellW + 20:
             return 'W'
-        if compass[0] > self.region.w - self.cellW - 10:
+        if compass[0] > self.region.w - self.cellW - 20:
             return 'E'
         return '?'
+
+    def updateCoordFromDir(self, curX, curY, dir):
+        if dir == 'N':
+            curY -= 1
+        elif dir == 'S':
+            curY += 1
+        elif dir == 'W':
+            curX -= 1
+        elif dir == 'E':
+            curX += 1
+        return (curX, curY)
 
     def navigate(self):
         coord = self.lookatCoord()
@@ -175,8 +192,8 @@ class Game:
         comp = self.locateCompasses()
         i = 0
         for c in comp:
-            print("{}: {} ({},{})".format(
-                i, self.findCompassDir(c), c[0], c[1]))
+            cellId = (int(c[0] / self.cellW), int(c[1] / self.cellH))
+            print("{}: {}\t{}".format(i, self.findCompassDir(c), cellId))
             i += 1
         while True:
             inp = input('> ')
@@ -188,14 +205,7 @@ class Game:
         pyautogui.click(
             self.region.x + comp[navTo][0], self.region.y + comp[navTo][1])
 
-    def autopilot(self, toX, toY):
-        coord = self.lookatCoord()
-        if coord is None:
-            print("Failed to extract coordonates")
-        else:
-            print('Coord: {}, {}'.format(coord[0], coord[1]))
-
-    def waitForMapChange(self, timeout=10000):
+    def waitForMapChange(self, timeout=5):
         startTime = int(time.time())
         pixel = pyautogui.pixel(
             self.region.x + self.region.w / 2, self.region.y + self.region.h / 3)
@@ -206,8 +216,136 @@ class Game:
                 time.sleep(1)
                 return
             if int(time.time()) - startTime > timeout:
+                print("[Game]Map change timeout")
                 return
-            time.sleep(0.05)
+            # time.sleep(0.005)
+
+
+class AutoPilot:
+    def __init__(self, game):
+        print("[AP]Init...")
+        self.game = game
+        self.setCurCoord()
+        self.histo = []
+        self.realCoord = []
+
+    def setCurCoord(self):
+        coord = self.game.lookatCoord()
+        if coord is None:
+            raise Exception("Could not find current coordinates")
+        self.curX = coord[0]
+        self.curY = coord[1]
+        print(f"[AP]Started at ({coord[0]}, {coord[1]})")
+
+    def checkCurCoord(self):
+        coord = self.game.lookatCoord()
+        if coord is None:
+            return 0
+        if coord[0] != self.curX or coord[1] != self.curY:
+            print(
+                f"[AP]Real Coords seems to be {coord}, expected {self.curX, self.curY}")
+            self.realCoord.append(coord)
+            if len(self.realCoord) >= 2:
+                self.correctCoord()
+        else:
+            self.realCoord = []
+
+        return 1
+
+    def correctCoord(self):
+        for i in range(1, len(self.realCoord)):
+            diffX = abs(self.realCoord[i-1][0] - self.realCoord[i][0])
+            diffY = abs(self.realCoord[i-1][1] - self.realCoord[i][1])
+            if diffX > 1 or diffY > 1 or (diffX >= 1 and diffY >= 1):
+                print("[AP]Removing odd values from RC")
+                del self.realCoord[0]
+                return
+        print(
+            f"[AP]Correcting coord from ({self.curX}, {self.curY}) to {self.realCoord[-1]}")
+        self.curX = self.realCoord[-1][0]
+        self.curY = self.realCoord[-1][1]
+        self.realCoord = []
+
+    def getDirPrio(self, dir):
+        dirPrios = {
+            'N': ['N', 'E', 'W', 'S'],
+            'E': ['E', 'S', 'N', 'W'],
+            'S': ['S', 'W', 'E', 'N'],
+            'W': ['W', 'N', 'S', 'E']
+        }
+        return (dirPrios[dir])
+
+    def getOppDir(self, dir):
+        oppDir = {
+            'N': 'S',
+            'S': 'N',
+            'E': 'W',
+            'W': 'E'
+        }
+        return oppDir[dir]
+
+    def updateState(self, dir):
+        self.histo.append(dir)
+        if dir == 'N':
+            self.curY -= 1
+        elif dir == 'S':
+            self.curY += 1
+        elif dir == 'W':
+            self.curX -= 1
+        elif dir == 'E':
+            self.curX += 1
+        print(f"[AP]believes to be at ({self.curX}, {self.curY})")
+
+    def gotoDir(self, compass, dir):
+        self.updateState(dir)
+        pyautogui.click(self.game.region.x +
+                        compass[0], self.game.region.y + compass[1])
+        self.game.waitForMapChange()
+        self.checkCurCoord()
+
+    def goto(self, dir):
+        comp = self.game.locateCompasses()
+        compMap = {}
+        for c in comp:
+            compMap[self.game.findCompassDir(c)] = c
+        dirPrio = self.getDirPrio(dir)
+        for d in dirPrio:
+            if len(self.histo) > 1 and d == self.getOppDir(self.histo[-1]):
+                continue
+            if d in compMap:
+                self.gotoDir(compMap[d], d)
+                return
+        print("[AP] seems stucked:")
+        print(self.histo)
+
+    def chooseDir(self, toX, toY):
+        diffX = abs(toX - self.curX)
+        diffY = abs(toY - self.curY)
+        if diffX > diffY:
+            if (toX > self.curX):
+                return 'E'
+            else:
+                return 'W'
+        else:
+            if (toY > self.curY):
+                return 'S'
+            else:
+                return 'N'
+
+    def navigateTo(self, toX, toY):
+        print(f"[AP] Set to ({toX}, {toY})")
+        while self.curY != toY or self.curX != toX:
+            dir = self.chooseDir(toX, toY)
+            self.goto(dir)
+        print('Arrived')
+        print(self.histo)
+        exit(0)
+
+    def menu(self):
+        print('Where to ?')
+        x = input('x> ')
+        y = input('y> ')
+        self.navigateTo(int(x), int(y))
 
 
 try:
@@ -216,9 +354,12 @@ except Exception as e:
     print(e)
     exit(1)
 
+pilot = AutoPilot(game)
+pilot.menu()
+
 # game.region.show(1)
 # game.lookatCoord()
 # game.gotoCell(13, 0)
-while True:
-    game.navigate()
-    game.waitForMapChange()
+# while True:
+#     game.navigate()
+#     game.waitForMapChange()
